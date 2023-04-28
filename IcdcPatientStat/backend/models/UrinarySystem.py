473bb5868bec9a179b5fe0a7e7b37b22bef6ea62
Mansur <magl.galimov@gmail.com>
2023-04-28 15:50:02 +0300

    .

--- IcdcPatientStat/backend/models/UrinarySystem.py
+++ IcdcPatientStat/backend/models/UrinarySystem.py
@@ -11,8 +11,7 @@ class UrinarySystem(TimeBasedModel):
         Patient,
         on_delete=models.CASCADE,
         related_name="urin_collection",
-        verbose_name="Пациент",
-        to_field="patient_id",
-    )
+        verbose_name="Пациент",    
+        )
     diurez = models.IntegerField("Суточный диурез")
     infuzyy = models.IntegerField("Инфузии")
